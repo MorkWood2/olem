cf68798e26ff65e98e8f42dd7fddc84f200260a3
Mark Wild <mark.wild189@gmail.com>
2020-07-01 14:23:01 -0700

    minor styles changes

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,37 +2,41 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl:
+        'https://i.ibb.co/yPS4jWc/ahmed-syed-6-NVr-H0-HB-DE-unsplash.jpg',
       id: 1,
-      linkUrl: 'shop/hats'
+      linkUrl: 'shop/hats',
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl:
+        'https://i.ibb.co/CWr9XVq/dami-adebayo-k6a-Qzm-Ib-R1s-unsplash.jpg',
       id: 2,
-      linkUrl: 'shop/jackets'
+      linkUrl: 'shop/jackets',
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/wwV9sfr/pat-kwon-EJTjetc8t-Ps-unsplash.jpg',
       id: 3,
-      linkUrl: 'shop/sneakers'
+      linkUrl: 'shop/sneakers',
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl:
+        'https://i.ibb.co/VHMNjvM/kimson-doan-HD8-Kly-WRYYM-unsplash.jpg',
       size: 'large',
       id: 4,
-      linkUrl: 'shop/womens'
+      linkUrl: 'shop/womens',
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl:
+        'https://i.ibb.co/Y30wTYD/donn-gabriel-baleva-U-Z4-P2-H3-KFE-unsplash.jpg',
       size: 'large',
       id: 5,
-      linkUrl: 'shop/mens'
-    }
-  ]
+      linkUrl: 'shop/mens',
+    },
+  ],
 };
 
 const directoryReducer = (state = INITIAL_STATE, action) => {
